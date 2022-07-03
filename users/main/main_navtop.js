3ab37a55b78bc8c02248b53812f50840eacb7ec5
박해찬 <gocks4578@naver.com>
2022-07-03 10:31:04 +0900

    박해찬 main

---
+++ users/main/main_navtop.js
@@ -0,0 +1,11 @@
+//어디든지 클릭시에 실행 
+$('.ffc0w66').on('click',function(){
+    // 상단 메뉴바 중  공통사항 
+    $('.b1t24mo5').attr('class','b12rw3s8');
+    $('.c1oqx6sn').attr('class','ceyi5b');
+
+    //여행지에 블록효과 css 적용 
+    $('.ix85u26').attr('class','i1eek2sl');
+    // 여행지에 지역으로 검색 드롭다운 보여줌
+    $('.p1jg7hjv').addClass('p5mmcz9');
+});
