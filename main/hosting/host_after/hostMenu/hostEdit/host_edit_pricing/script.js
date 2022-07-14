588f628b90e117a3550ed4f9dbf8f544f10342d3
최종복 <21_43_65@naver.com>
2022-07-14 16:04:21 +0900

    최종복

--- main/hosting/host_after/hostMenu/hostEdit/host_edit_pricing/script.js
+++ main/hosting/host_after/hostMenu/hostEdit/host_edit_pricing/script.js
@@ -167,6 +167,142 @@ $('#cnlth7').on('click',function(){
     $("#wjwkd7").attr("disabled", true)
 })
 /*체크인 요일제한 셀렉트 저장버튼 활성 눌렀을 때*/ 
-$('#select7').on("change",function(){
-    $("#wjwkd6").attr("disabled", false)
+$('._1yf4i4f').on("change",function(){
+    $("#wjwkd7").attr("disabled", false)
 })
+
+/*------------------------------------------------------------------------------------*/
+/* 피드백 팝업 on*/
+$('#popup44').on('click',function(){
+    $('#popupgg').show()
+})
+/* 피드백 팝업 off*/
+$('#button4321').on('click',function(){
+    $('#popupgg').hide()
+    $('#button642').attr("disabled", true);
+})
+
+$('.pop0').on('click',function(){
+    $('#button642').attr("disabled", false);
+})
+
+/*------------------------------------------------------------------------------------*/
+
+/*맞춤숙박 기간 수정하기 버튼 눌렀을 때*/ 
+$('#su8').on('click',function(){
+    $('#first_8').attr('class', 'first_8');
+    $('#susu8').attr('class', 'susu8 _kkkkkkk');
+})
+/*맞춤숙박 기간 닫기 버튼 눌렀을 때*/ 
+$('#ekerl8').on('click',function(){
+    $('#first_8').attr('class', 'first_8 _kkkkkkk');
+    $('#susu8').attr('class', 'susu8');
+    $("#wjwkd8").attr("disabled", true)
+})
+/*맞춤숙박 기간 취소 버튼 눌렀을 때*/ 
+$('#cnlth8').on('click',function(){
+    $('#first_8').attr('class', 'first_8 _kkkkkkk');
+    $('#susu8').attr('class', 'susu8');
+    $("#wjwkd8").attr("disabled", true)
+})
+/*맞춤숙박 기간 셀렉트 저장버튼 활성 눌렀을 때*/ 
+$('._1yf4i4f').on("change",function(){
+    $("#wjwkd8").attr("disabled", false)
+})
+/*셀렉트 확인 */
+
+
+/*맞춤숙박 기간 수정하기 버튼 눌렀을 때*/ 
+$('#cnrkgkrl').on('click',function(){
+    $('#first_9').attr('class', 'first_9');
+    $('#first_8').attr('class', 'first_8 _kkkkkkk');
+})
+/*맞춤숙박 기간 삭제 버튼 눌렀을 때*/ 
+$('#tkrwp').on('click',function(){
+    $('#first_9').attr('class', 'first_9');
+    $('#first_8').attr('class', 'first_8 _kkkkkkk');
+})
+/*맞춤숙박 기간 닫기 버튼 눌렀을 때*/ 
+$('#ekerl9').on('click',function(){
+    $('#first_9').attr('class', 'first_9 _kkkkkkk');
+    $('#susu8').attr('class', 'susu8');
+    $("#wjwkd9").attr("disabled", true)
+})
+/*맞춤숙박 기간 취소 버튼 눌렀을 때*/ 
+$('#cnlth9').on('click',function(){
+    $('#first_9').attr('class', 'first_9 _kkkkkkk');
+    $('#susu8').attr('class', 'susu8');
+    $("#wjwkd9").attr("disabled", true)
+})
+
+/*------------------------------------------------------------------------------------*/
+
+/*최소숙박 기간 수정하기 버튼 눌렀을 때*/ 
+$('#su10').on('click',function(){
+    $('#first_10').attr('class', 'first_10');
+    $('#susu10').attr('class', 'susu10 _kkkkkkk');
+})
+/*최소숙박 기간 닫기 버튼 눌렀을 때*/ 
+$('#ekerl10').on('click',function(){
+    $('#first_10').attr('class', 'first_10 _kkkkkkk');
+    $('#susu10').attr('class', 'susu10');
+    $("#wjwkd10").attr("disabled", true)
+})
+/*최소숙박 기간 취소 버튼 눌렀을 때*/ 
+$('#cnlth10').on('click',function(){
+    $('#first_10').attr('class', 'first_10 _kkkkkkk');
+    $('#susu10').attr('class', 'susu10');
+    $("#wjwkd10").attr("disabled", true)
+})
+/*최소숙박 기간 셀렉트 저장버튼 활성 눌렀을 때*/ 
+$('._yrzsibu').on("change",function(){
+    $("#wjwkd10").attr("disabled", false)
+})
+/*셀렉트 확인 */
+
+
+/*최소숙박 맞춤설정 버튼 눌렀을 때*/ 
+$('#chlth').on('click',function(){
+    $('#first_11').attr('class', 'first_11');
+    $('#first_10').attr('class', 'first_10 _kkkkkkk');
+})
+
+/*최소숙박 맞춤설정 닫기 버튼 눌렀을 때*/ 
+$('#ekerl11').on('click',function(){
+    $('#first_11').attr('class', 'first_11 _kkkkkkk');
+    $('#susu10').attr('class', 'susu10');
+    $("#wjwkd11").attr("disabled", true)
+})
+/*최소숙박 맞춤설정 취소 버튼 눌렀을 때*/ 
+$('#cnlth11').on('click',function(){
+    $('#first_11').attr('class', 'first_11 _kkkkkkk');
+    $('#susu10').attr('class', 'susu10');
+    $("#wjwkd11").attr("disabled", true)
+})
+$('._c5rhl5').on("change",function(){
+    $("#wjwkd11").attr("disabled", false)
+})
+/*------------------------------------------------------------------------------------*/
+
+/*추가게스트 수정하기 버튼 눌렀을 때*/ 
+$('#su12').on('click',function(){
+    $('#first_12').attr('class', 'first_12');
+    $('#susu12').attr('class', 'susu12 _kkkkkkk');
+})
+/*추가게스트 닫기 버튼 눌렀을 때*/ 
+$('#ekerl12').on('click',function(){
+    $('#first_12').attr('class', 'first_12 _kkkkkkk');
+    $('#susu12').attr('class', 'susu12');
+    $("#wjwkd12").attr("disabled", true)
+})
+/*추가게스트 취소 버튼 눌렀을 때*/ 
+$('#cnlth12').on('click',function(){
+    $('#first_12').attr('class', 'first_12 _kkkkkkk');
+    $('#susu12').attr('class', 'susu12');
+    $("#wjwkd12").attr("disabled", true)
+})
+/*추가게스트 셀렉트 저장버튼 활성 눌렀을 때*/ 
+$('._1jg0g92a').on("change",function(){
+    $("#wjwkd12").attr("disabled", false)
+})
+
